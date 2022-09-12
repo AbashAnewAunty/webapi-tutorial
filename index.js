16d8e16ca9c71508de3b05725eb0355a32eac519
AbashAnewAunty28 <fuwarinn.ponpon@gmail.com>
2022-09-13 00:46:28 +0900

    develop

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ app.listen(3000, console.log("サーバーが開始されました"));
 app.get("/", (req, res) => {
     res.send("プログラミングチュートリアルへようこそ");
 });
+
+// お客様情報をサーバーに置いておく
+const customers = [
+    {title: "田中", id : 1},
+    {title: "鈴木", id : 2},
+    {title: "橋本", id : 3},
+    {title: "安田", id : 4},
+    {title: "森田", id : 5},
+];
+
+// データを取得できるようにしよう(GETメソッド)
+app.get("/api/customers/", (req, res) =>{
+    res.send(customers);
+});
