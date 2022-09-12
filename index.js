709f1ecdae524dc85ddca5d914ce9514ab84a41e
AbashAnewAunty28 <fuwarinn.ponpon@gmail.com>
2022-09-13 01:23:19 +0900

    develop

--- index.js
+++ index.js
@@ -43,3 +43,11 @@ app.put("/api/customers/:id", (req, res) => {
     customer.title = req.body.title;
     res.send(customer);
 })
+
+// データを削除してみよう(DELETEメソッド)
+app.delete("/api/customers/:id", (req, res) => {
+    const customer = customers.find((c) => c.id === parseInt(req.params.id));
+    const index = customers.indexOf(customer);
+    customers.splice(index, 1);
+    res.send(customer);
+})
